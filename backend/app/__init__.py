76d2d8a5cc5e700fa498d41bd969d4e303426c69
Rachel Pratt <rachlpratt@gmail.com>
2023-10-24 21:18:41 -0400

    Debugging Firestore issue

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask, jsonify
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import os
 
 app = Flask(__name__)
 
-cred = credentials.Certificate('firestore_sa.json')
+abs_cred_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             'firestore_sa.json')
+
+cred = credentials.Certificate(abs_cred_path)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
